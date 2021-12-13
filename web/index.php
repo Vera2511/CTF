5915fd8d29ddcca9fcee8659817a3af3eb8433e4
Антон Григорьев <grianton535@gmail.com>
2021-12-14 01:31:33 +0300

    changed main page, changed registry

--- web/index.php
+++ web/index.php
@@ -1,44 +1,112 @@
 <?php
 include '/var/www/ctf/src/core.php';
 includeTemplate('header.php', ['title' => 'Главная', '_SESSION' => $_SESSION]);
+if ($_SESSION) {
 $id = $_SESSION['team_id'];
 $onRoute = $pdo->getData("SELECT on_route FROM teams WHERE team_id = $id")[0]['on_route'];
-$top[] = $pdo->getData("select top_score(3)");
-$top[] = $pdo->getData("select top_score(5)");
-$top[] = $pdo->getData("select top_score(7)");
+}
+$top1 = $pdo->getData("select top_score(3) limit 3");
+$top2 = $pdo->getData("select top_score(5) limit 3");
+$top3 = $pdo->getData("select top_score(7) limit 3");
+
 ?>
 <main class="container-xxl">
-<h1>Главная</h1>
-<h5>Топ 3</h5>
-
-<?php foreach ($top as $value) { ?>
-<table>
-	<thead>
-		<th>Название команды</th>
-		<th>Счет</th>
-	</thead>
-	<tbody>
+</br></br></br>
+    <main class="container-xxl">
+        <div class="col-md-4 float-md-end mb-3 ms-md-3 p-3" style="background-color: white; border-radius: 4px;">
+            <h5 class="text-center">Топ 3</h5>
+            <nav>
+                <div class="nav nav-tabs d-flex justify-content-evenly" id="nav-tab" role="tablist">
+                    <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">3 игрока</button>
+                    <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">5 игроков</button>
+                    <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">7 игроков</button>
+                </div>
+            </nav>
+            <div class="tab-content" id="nav-tabContent">
+				<div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+					<table class="table">
+						<thead>
+							<th>Имя</th>
+							<th>Счет</th>
+						</thead>
+						<tbody>
 <?php 
-foreach ($value as $team) {
+foreach ($top1 as $team) {
 	$team = explode(',', str_replace(['(', ')'], '', $team['top_score']));
 	$teamName = $team[0];
 	$teamScore = $team[1];
 ?>
-<tr>
-<td><?=$teamName?></td>
-<td><?=$teamScore?></td>
-</tr>
+							<tr>
+								<td><?=$teamName?></td>
+								<td><?=$teamScore?></td>
+							</tr>
 <?php } ?>
-	</tbody>
-</table>
-<?php } 
-if ($onRoute) {
-	$buttonText = 'Продолжить выполнение заданий';
+						</tbody>
+					</table>
+				</div>
+				<div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+					<table class="table">
+						<thead>
+							<th>Имя</th>
+							<th>Счет</th>
+						</thead>
+						<tbody>
+<?php 
+foreach ($top2 as $team) {
+	$team = explode(',', str_replace(['(', ')'], '', $team['top_score']));
+	$teamName = $team[0];
+	$teamScore = $team[1];
+?>
+							<tr>
+								<td><?=$teamName?></td>
+								<td><?=$teamScore?></td>
+							</tr>
+<?php } ?>
+						</tbody>
+					</table>
+				</div>
+				<div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+					<table class="table">
+						<thead>
+							<th>Имя</th>
+							<th>Счет</th>
+						</thead>
+						<tbody>
+<?php 
+foreach ($top3 as $team) {
+	$team = explode(',', str_replace(['(', ')'], '', $team['top_score']));
+	$teamName = $team[0];
+	$teamScore = $team[1];
+?>
+							<tr>
+								<td><?=$teamName?></td>
+								<td><?=$teamScore?></td>
+							</tr>
+<?php } ?>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+<?php
+if (isset($onRoute)) {
+	if ($onRoute) {
+		$buttonText = 'Продолжить выполнение заданий';
+	} else {
+		$buttonText = 'Начать выполнение заданий';
+	}
+	$buttonHref = '/task/';
 } else {
-	$buttonText = 'Начать выполнение заданий';
+	$buttonText = 'Начать';
+	$buttonHref = '/registry';
 }
 ?>
-<button onclick="location.href = '/task/'"><?=$buttonText?></button>
+		<div class="m-5">
+            Формат Сapture the Flag используется в пейнтболе, среди ролевиков, в компьютерных играх и в информационной безопасности. Сapture the Flag или CTF в ИБ — это соревнования в форме командной игры, главная цель которой — захватить «флаг» у соперника в приближенных к реальности условиям. Команды решают прикладные задачи, чтобы получить уникальную комбинацию символов (флаг). Далее участники отправляют флаг в специальную платформу и получают подтверждение, что задача решена верно или стоит попытаться дать ответ ещё раз.
+        </div>
+        <div class="text-center">
+            <button onclick="location.href = '<?=$buttonHref?>'" class="btn btn-primary btn-lg p-4" style="font-size:24pt"><?=$buttonText?></button>
+        </div>
 </main>
 <?php
 includeTemplate('footer.php');
--- web/registry/index.php
+++ web/registry/index.php
@@ -6,22 +6,35 @@ if (!empty($_POST)) {
   $count = pg_escape_string($_POST['count']);
   $login = pg_escape_string($_POST['login']);
   $password = pg_escape_string($_POST['password']);
-  $query = $pdo->query("INSERT INTO public.teams (team_name, team_size, score, on_route, reg_date)
-    VALUES ('$name', $count, 0, false, now()::date);"
-  );
-  if ($query) {
-    $result = $pdo->query("INSERT INTO public.login_data (login, pass) VALUES ('$login', '$password');");
-    if ($result) {
-      $success = "Вы успешно зарегистрированны";
-      header('refresh:2, url=/login/');
+  $exist = False;
+  $teams = $pdo->getData("SELECT login, team_name FROM login_data INNER JOIN teams ON teams.team_id = login_data.team_id");
+  foreach ($teams as $team) {
+    if ($name == $team['team_name'] || $login == $team['login']) {
+      $exist = True;
+      break;
+    }
+  }
+  if (!$exist) {
+    $query = $pdo->query("INSERT INTO public.teams (team_name, team_size, score, on_route, reg_date)
+      VALUES ('$name', $count, 0, false, now()::date);"
+    );
+    if ($query) {
+      $result = $pdo->query("INSERT INTO public.login_data (login, pass) VALUES ('$login', '$password');");
+      if ($result) {
+        $success = "Вы успешно зарегистрированны";
+        header('refresh:2, url=/login/');
+      } else {
+        $error = 'Ошибка БД';
+        $pdo->query("delete from teams where team_name = '$name' AND team_size = $count;");
+      }
     } else {
       $error = 'Ошибка БД';
-      $pdo->query("delete from teams where team_name = '$name' AND team_size = $count;");
     }
   } else {
-    $error = 'Ошибка БД';
+    $error = 'Такая команда уже существует';
   }
 }
+
 
 includeTemplate('header.php', ['title' => 'Регистрация', '_SESSION' => $_SESSION]);
 if (isset($success)) {
@@ -35,13 +48,20 @@ if (isset($success)) {
     <form method="post" class="container col-3 text-center ">
       <h1 class="h3 mb-3 fw-normal">Регистрация</h1>
       <h2 class="h4 mb-3 fw-normal">Информация о команде</h1>
-      	<div class="form-floating">
+      <div class="form-floating">
         <input type="text" class="form-control" id="floatingInput" placeholder="Название команды" name='name' required>
         <label for="floatingInput">Название команды</label>
       </div>
       <div class="form-floating">
-        <input type="number" class="form-control" id="floatingInput" placeholder="Количество человек" name='count' required>
-        <label for="floatingInput">Количество человек</label>
+        <p>Количество человек</p>
+        <div class="text-center">
+          <input type="radio" class="btn-check" name="count" id="option1" autocomplete="off" value="3">
+          <label class="btn btn-outline-primary btn-sm mx-2" for="option1">3</label>
+          <input type="radio" class="btn-check" name="count" id="option2" autocomplete="off" value="5">
+          <label class="btn btn-outline-primary btn-sm mx-2" for="option2">5</label>
+          <input type="radio" class="btn-check" name="count" id="option3" autocomplete="off" value="7">
+          <label class="btn btn-outline-primary btn-sm mx-2" for="option3">7</label>
+        </div>
       </div>
       <hr>
       <h2 class="h4 mb-3 fw-normal">Аутентификация</h1>
@@ -54,7 +74,9 @@ if (isset($success)) {
         <label for="floatingPassword">Пароль</label>
       </div>
       <button class="w-100 btn btn-lg btn-primary my-2" type="submit">Зарегистрироваться</button>
+      <a href="/login/">У меня уже есть аккаунт</a>
     </form>
+
 <?php
 }
 includeTemplate('footer.php');
